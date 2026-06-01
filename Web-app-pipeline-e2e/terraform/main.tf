ba4e271784bc2e9e45f27d060b0f0a3e5613f1ef
harishmsgit <senharishms108@gmail.com>
2026-06-01 22:21:56 +0530

    Reduce EKS node group to a single worker for quota-safe deployment

--- Web-app-pipeline-e2e/terraform/main.tf
+++ Web-app-pipeline-e2e/terraform/main.tf
@@ -222,8 +222,8 @@ resource "aws_eks_node_group" "workers" {
   subnet_ids      = aws_subnet.public[*].id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
