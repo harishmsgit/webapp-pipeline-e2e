b83f69ce697dc55b0749018f75dd7c7395d0c8d1
harishmsgit <senharishms108@gmail.com>
2026-06-07 02:10:32 +0530

    update scaling_config

--- Web-app-pipeline-e2e/terraform/main.tf
+++ Web-app-pipeline-e2e/terraform/main.tf
@@ -318,8 +318,8 @@ resource "aws_eks_node_group" "workers" {
   subnet_ids      = aws_subnet.public[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 4
     min_size     = 1
   }
 
